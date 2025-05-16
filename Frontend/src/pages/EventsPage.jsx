6e908f7afb16d7f0b273e15a5088fab2f43d9295
shalith98 <shalithalakruwan1998@gmail.com>
2025-05-16 14:58:49 +0530

    Enhance Ux when loading list of evennts.

--- Frontend/src/pages/EventsPage.jsx
+++ Frontend/src/pages/EventsPage.jsx
@@ -1,24 +1,34 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Await } from "react-router-dom";
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
+import classes from "../components/PageContent.module.css";
 
 function EventsPage() {
   const data = useLoaderData();
 
   return (
-    <>
-      <EventsList events={data.events} />
-    </>
+    <Suspense fallback={<p className={classes.content + " " + classes.highlight_text}> {"Loading..."}</p>}>
+      <Await resolve={data.events}>{(loadedEvent) => <EventsList events={loadedEvent.events} />}</Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
 // eslint-disable-next-line react-refresh/only-export-components
-export async function loader() {
+export async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
     throw new Response(JSON.stringify({ message: "An Error Occured!" }, { status: response.status }));
   }
 
-  return response;
+  const data = await response.json();
+
+  return data;
+}
+
+export async function loader() {
+  return {
+    events: loadEvents(),
+  };
 }
