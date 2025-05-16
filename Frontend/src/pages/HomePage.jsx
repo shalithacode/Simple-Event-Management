530654480c2a40f6abb6c22b882987894a16a378
shalith98 <shalithalakruwan1998@gmail.com>
2025-05-17 00:14:59 +0530

    Added simple home page

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import PageContent from "../components/PageContent";
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <PageContent title="Welcome">
+      <p>Browse All our amazing events.</p>
+    </PageContent>
+  );
 }
 
 export default HomePage;
